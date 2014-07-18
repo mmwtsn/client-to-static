00866d5551391b6b28f7f8af22a40d846eb54741
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-18 13:56:48 -0400

    calls local cms api

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'sinatra/export'
 
+require 'net/http'
+require 'json'
+
+response = Net::HTTP.get('localhost', '/pages/1/preview.json', '3000')
+data = JSON.parse(response)
+
 get '/' do
   erb :home
 end
